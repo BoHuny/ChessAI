40368b480cf8035c6577d4aca2423a94ad327e18
Benjamin <Benjamin.Boulle@etu.uphf.fr>
2022-07-06 20:18:03 +0200

    refactor: created main package

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ChessAI
+# main.ChessAI
 This is a draft of a basic chess AI using MCTS
--- src/main/ChessAI.java
+++ src/main/ChessAI.java
@@ -1,3 +1,5 @@
+package main;
+
 public class ChessAI {
     public static void main(String[] args) {
         System.out.println("Chess AI");
